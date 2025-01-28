3b4a2325cece7b3524066c06726f8656b5eea283
Fabio Spampinato <spampinabio@gmail.com>
2025-01-28 23:41:45 +0000

    Added a basic test suite

---
+++ test/index.js
@@ -0,0 +1,18 @@
+
+/* IMPORT */
+
+import open from '../dist/index.js';
+
+/* MAIN */
+
+const URL = 'https://example.com';
+
+const chrome = await open ( URL, { app: 'chrome' } );
+const firefox = await open ( URL, { app: 'firefox' } );
+const edge = await open ( URL, { app: 'edge' } );
+const safari = await open ( URL, { app: 'safari' } );
+
+console.log ( 'chrome', chrome );
+console.log ( 'firefox', firefox );
+console.log ( 'edge', edge );
+console.log ( 'safari', safari );
